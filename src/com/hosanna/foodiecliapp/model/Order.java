69456bffaf866f3e9c2ca8231ecaf2ef89df90f9
Prem-Thomas <erapogupremaraju158200@gmail.com>
2024-07-27 20:05:50 +0530

    feat: Implemented Dish,Order

--- src/com/hosanna/foodiecliapp/model/Order.java
+++ src/com/hosanna/foodiecliapp/model/Order.java
@@ -1,5 +1,6 @@
 package com.hosanna.foodiecliapp.model;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,8 +28,10 @@ public class Order {
     private String id;
     private Customer customer;
     private Restaurant restaurant;
-    private List<Dish>  dishes;
-    private double price;
+    private List<Dish>  dishList;
+    private double totalPrice;
+    private LocalDate orderDate;
+
 
     public Order() {
     }
@@ -37,40 +40,54 @@ public class Order {
         return id;
     }
 
-    public void setId(String id) {
+    public Order setId(String id) {
         this.id = id;
+        return this;
     }
 
     public Customer getCustomer() {
         return customer;
     }
 
-    public void setCustomer(Customer customer) {
+    public Order setCustomer(Customer customer) {
         this.customer = customer;
+        return this;
     }
 
     public Restaurant getRestaurant() {
         return restaurant;
     }
 
-    public void setRestaurant(Restaurant restaurant) {
+    public Order setRestaurant(Restaurant restaurant) {
         this.restaurant = restaurant;
+        return this;
     }
 
-    public List<Dish> getDishes() {
-        return dishes;
+    public List<Dish> getDishList() {
+        return dishList;
     }
 
-    public void setDishes(List<Dish> dishes) {
-        this.dishes = dishes;
+    public Order setDishList(List<Dish> dishList) {
+        this.dishList = dishList;
+        return this;
     }
 
-    public double getPrice() {
-        return price;
+    public double getTotalPrice() {
+        return totalPrice;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
+    public Order setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+        return this;
+    }
+
+    public LocalDate getOrderDate() {
+        return orderDate;
+    }
+
+    public Order setOrderDate(LocalDate orderDate) {
+        this.orderDate = orderDate;
+        return this;
     }
 
     @Override
@@ -78,12 +95,12 @@ public class Order {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Order order = (Order) o;
-        return Double.compare(price, order.price) == 0 && Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(restaurant, order.restaurant) && Objects.equals(dishes, order.dishes);
+        return Double.compare(totalPrice, order.totalPrice) == 0 && Objects.equals(id, order.id) && Objects.equals(customer, order.customer) && Objects.equals(restaurant, order.restaurant) && Objects.equals(dishList, order.dishList) && Objects.equals(orderDate, order.orderDate);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, customer, restaurant, dishes, price);
+        return Objects.hash(id, customer, restaurant, dishList, totalPrice, orderDate);
     }
 
     @Override
@@ -92,8 +109,9 @@ public class Order {
                 "id='" + id + '\'' +
                 ", customer=" + customer +
                 ", restaurant=" + restaurant +
-                ", dishes=" + dishes +
-                ", price=" + price +
+                ", dishList=" + dishList +
+                ", totalPrice=" + totalPrice +
+                ", orderDate=" + orderDate +
                 '}';
     }
 }
